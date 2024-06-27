bbf5ee77b753a82cd8e7f8b02a49ff8ebe1c19f9
vik-bit <vikrammohanty.royalstar12@gmail.com>
2024-06-27 17:29:34 +0530

    build added

---
+++ build/precache-manifest.c4b4e321a34a1279fbcf6f5ec3aa0b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9e9c5e43efb3bf7153a382e4311e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4f8d83a9a54520b6fda",
+    "url": "/static/css/main.5062ff3c.chunk.css"
+  },
+  {
+    "revision": "1d1f060aad56c4bbdf1e",
+    "url": "/static/js/2.d370199a.chunk.js"
+  },
+  {
+    "revision": "4704814cfbfc26d598a03749ded69371",
+    "url": "/static/js/2.d370199a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f8d83a9a54520b6fda",
+    "url": "/static/js/main.966fc429.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
